9620a71913cb0dac0e74d1ef2a68eff02c10ab7e
I, Erik <31218827+erik-iglikov@users.noreply.github.com>
2022-09-07 17:35:50 -0400

    Recursively go through sub-folders

--- Find-Replace Missing Items.jsx
+++ Find-Replace Missing Items.jsx
@@ -1,28 +1,58 @@
-﻿function replaceMissingFiles(projectFolderItem, searchFolderPath) {
-        var thisFolder = projectFolderItem;
-        var resFolder = Folder(searchFolderPath);
-        var subFolder, subSubFolder;
+/** 
+ * This script search missing footage items and replaces them if found in a given folder.
+ * 
+ * 
+ * Command to run on Windows (NOTE: Always use full paths!)
+ * PowerShell:
+ * & "C:\Program Files\Adobe\Adobe After Effects 2022\Support Files\AfterFX.exe" -noui -r "C:\Users\Administrator\Desktop\alert.js"
+ * Command Prompt
+ * "C:\Program Files\Adobe\Adobe After Effects 2022\Support Files\AfterFX.exe" -noui -ro "C:\Users\Administrator\Desktop\alert.js"
+ *
+ *
+ * On Mac
+ * osascript -e 'tell application "Adobe After Effects 2021" to activate' -e 'tell application "Adobe After Effects 2021" to DoScriptFile "/path/to/my script.jsx"'
+ * 
+ */
 
-            for(var i = 1; i <= thisFolder.numItems; i++) {
-                if(thisFolder.item(i) instanceof FootageItem) {
-                                if(thisFolder.item(i).footageMissing == true) {
-                                findAndReplaceMissingFootage(thisFolder.item(i), resFolder);
-                                }
-                }
-            
-    }
-}
+function replaceMissingFiles(projectFolder, searchFolderPath) {
+    var resFolder = Folder(searchFolderPath);
 
-function findAndReplaceMissingFootage(missingItem, resourceFolder) {
-    var foundItem;
-    var searchItems =resourceFolder.getFiles();
-    for(var i = 0; i < searchItems.length; i++) {
-        if(searchItems[i].fsName.replace(/%20/g, " " ).indexOf(missingItem.name.replace(/%20/g, " ")) != -1) {
-            missingItem.replace(searchItems[i]);
+    for (var i = 1; i <= projectFolder.numItems; i++) {
+        if (projectFolder.item(i) instanceof FootageItem) {
+            if (projectFolder.item(i).footageMissing === true) {
+                findAndReplaceMissingFootage(projectFolder.item(i), resFolder);
             }
         }
     }
+};
+
+function findAndReplaceMissingFootage(missingItem, resourceFolder) {
+    var searchItems = resourceFolder.getFiles();
+
+    for (var i = 0; i < searchItems.length; i++) {
+        if (searchItems[i] instanceof File) {
+            if (searchItems[i].fsName.replace(/%20/g, " ").indexOf(missingItem.name.replace(/%20/g, " ")) !== -1) {
+                missingItem.replace(searchItems[i]);
+            }
+        }
+    }
+    
+    // Check for folders, if found any - go into it
+    for (var j = 0; j < searchItems.length; j++) {
+        if (searchItems.length !== 0 && searchItems[j] instanceof Folder) {
+            findAndReplaceMissingFootage(missingItem, searchItems[j]);
+        }
+    }
+};
+
 
 app.beginUndoGroup("Replacing");
-replaceMissingFiles(app.project.item(1), "C:\\Users\\Nate\\Documents\\test");
+/**
+ * Mac path:
+ * /Users/erik/Music/template-files/video/story-promo folder/(Footage)
+ *
+ * Windows path:
+ * C:\\Users\\Administrator\\Desktop\\story-promo folder\\(Footage)
+  */
+replaceMissingFiles(app.project, "C:\\Users\\Administrator\\Desktop\\story-promo folder\\(Footage)");
 app.endUndoGroup();
